21f8e5ec60c0a54f3b60815da26c603fea36c303
MC洛洛 <1592859065@qq.com>
2024-06-22 03:52:31 +0800

    修复bwBUG
    修复“没有败场的玩家不会显示胜场和败场”及类似BUG

--- src/main/java/xyz/jxmm/minecraft/bw/BedWars.java
+++ src/main/java/xyz/jxmm/minecraft/bw/BedWars.java
@@ -24,7 +24,7 @@ public class BedWars {
         if (json.get("player").isJsonObject()) {
             playerJson = json.get("player").getAsJsonObject();
 
-            chain.append(new PlainText("\n"+ Nick.nick(playerJson) + " ")); //玩家名称前缀
+            chain.append(new PlainText("\n" + Nick.nick(playerJson) + " ")); //玩家名称前缀
             chain.append(new PlainText(json.get("player").getAsJsonObject().get("displayname").getAsString()));
             chain.append(new PlainText(" | 起床战争数据:"));
 
@@ -37,38 +37,66 @@ public class BedWars {
                     chain.append(new PlainText(" | 等级: "));
                     chain.append(new PlainText(String.valueOf(json.get("player").getAsJsonObject().get("achievements").getAsJsonObject().get("bedwars_level").getAsInt())));
                     if (bwJson.has("winstreak")) {
-                        chain.append(new PlainText(" | 当前连胜: "));
+                        chain.append(new PlainText(" | 连胜: "));
                         chain.append(new PlainText(String.valueOf(bwJson.get("winstreak").getAsInt())));
                     }
                 }
 
-                if (kills_bedwars(bwJson)) {
-                    chain.append(new PlainText("\n击杀数: "));
+                chain.append(new PlainText("\n胜场: "));
+                if (bwJson.has("wins_bedwars")) {
+                    chain.append(new PlainText(String.valueOf(bwJson.get("wins_bedwars").getAsInt())));
+                } else chain.append(new PlainText("0"));
+                chain.append(new PlainText(" | 败场: "));
+                if (bwJson.has("losses_bedwars")) {
+                    chain.append(new PlainText(String.valueOf(bwJson.get("losses_bedwars").getAsInt())));
+                } else chain.append(new PlainText("0"));
+                if (bwJson.has("wins_bedwars") && bwJson.has("losses_bedwars")) {
+                    chain.append(new PlainText(" | WLR: "));
+                    chain.append(new PlainText(decimalFormat.format(
+                            (float) bwJson.get("wins_bedwars").getAsInt() /
+                                    (float) bwJson.get("losses_bedwars").getAsInt())));
+                }
+
+                chain.append(new PlainText("\n击杀: "));
+                if (bwJson.has("kills_bedwars")) {
                     chain.append(new PlainText(String.valueOf(bwJson.get("kills_bedwars").getAsInt())));
-                    chain.append(new PlainText(" | 死亡数: "));
+                } else chain.append(new PlainText("0"));
+                chain.append(new PlainText(" | 死亡: "));
+                if (bwJson.has("deaths_bedwars")) {
                     chain.append(new PlainText(String.valueOf(bwJson.get("deaths_bedwars").getAsInt())));
+                } else chain.append(new PlainText("0"));
+                if (bwJson.has("kills_bedwars") && bwJson.has("deaths_bedwars")) {
                     chain.append(new PlainText(" | KDR: "));
                     chain.append(new PlainText(decimalFormat.format(
                             (float) bwJson.get("kills_bedwars").getAsInt() /
                                     (float) bwJson.get("deaths_bedwars").getAsInt())));
                 }
 
-                if (final_kills_bedwars(bwJson)) {
-                    chain.append(new PlainText("\n最终击杀: "));
+                chain.append(new PlainText("\n最终击杀: "));
+                if (bwJson.has("final_kills_bedwars")) {
                     chain.append(new PlainText(String.valueOf(bwJson.get("final_kills_bedwars").getAsInt())));
-                    chain.append(new PlainText(" | 最终死亡: "));
+                } else chain.append(new PlainText("0"));
+                chain.append(new PlainText(" | 最终死亡: "));
+                if (bwJson.has("final_deaths_bedwars")) {
                     chain.append(new PlainText(String.valueOf(bwJson.get("final_deaths_bedwars").getAsInt())));
+                } else chain.append(new PlainText("0"));
+                if (bwJson.has("final_kills_bedwars") && bwJson.has("final_kills_bedwars")) {
                     chain.append(new PlainText(" | FKDR："));
                     chain.append(new PlainText(decimalFormat.format(
                             (float) bwJson.get("final_kills_bedwars").getAsInt() /
                                     (float) bwJson.get("final_deaths_bedwars").getAsInt())));
                 }
 
-                if (kills_bedwars(bwJson) && final_kills_bedwars(bwJson)) {
-                    chain.append(new PlainText("\n总击杀: "));
+                chain.append(new PlainText("\n总击杀: "));
+                if (bwJson.has("kills_bedwars") && bwJson.has("final_kills_bedwars")) {
                     chain.append(new PlainText(String.valueOf(bwJson.get("kills_bedwars").getAsInt() + bwJson.get("final_kills_bedwars").getAsInt())));
-                    chain.append(new PlainText(" | 总死亡: "));
+                } else chain.append(new PlainText("0"));
+                chain.append(new PlainText(" | 总死亡: "));
+                if (bwJson.has("deaths_bedwars") && bwJson.has("final_deaths_bedwars")) {
                     chain.append(new PlainText(String.valueOf(bwJson.get("deaths_bedwars").getAsInt() + bwJson.get("final_deaths_bedwars").getAsInt())));
+                } else chain.append(new PlainText("0"));
+                if (bwJson.has("kills_bedwars") && bwJson.has("final_kills_bedwars") &&
+                        bwJson.has("deaths_bedwars") && bwJson.has("final_deaths_bedwars")) {
                     chain.append(new PlainText(" | 总KD: "));
                     chain.append(new PlainText(decimalFormat.format(
                             (float) (bwJson.get("kills_bedwars").getAsInt() + bwJson.get("final_kills_bedwars").getAsInt()) /
@@ -76,27 +104,21 @@ public class BedWars {
                     )));
                 }
 
-                if (beds_broken_bedwars(bwJson)) {
-                    chain.append(new PlainText("\n摧毁床: "));
+                chain.append(new PlainText("\n摧毁床: "));
+                if (bwJson.has("beds_broken_bedwars")) {
                     chain.append(new PlainText(String.valueOf(bwJson.get("beds_broken_bedwars").getAsInt())));
-                    chain.append(new PlainText(" | 被摧毁床: "));
+                } else chain.append(new PlainText("0"));
+                chain.append(new PlainText(" | 被摧毁床: "));
+                if (bwJson.has("beds_lost_bedwars")) {
                     chain.append(new PlainText(String.valueOf(bwJson.get("beds_lost_bedwars").getAsInt())));
+                } else chain.append(new PlainText("0"));
+                if (bwJson.has("beds_broken_bedwars") && bwJson.has("beds_lost_bedwars")) {
                     chain.append(new PlainText(" | BLR: "));
                     chain.append(new PlainText(decimalFormat.format(
                             (float) bwJson.get("beds_broken_bedwars").getAsInt() /
                                     (float) bwJson.get("beds_lost_bedwars").getAsInt())));
                 }
 
-                if (wins_bedwars(bwJson)) {
-                    chain.append(new PlainText("\n胜利数: "));
-                    chain.append(new PlainText(String.valueOf(bwJson.get("wins_bedwars").getAsInt())));
-                    chain.append(new PlainText(" | 失败数: "));
-                    chain.append(new PlainText(String.valueOf(bwJson.get("losses_bedwars").getAsInt())));
-                    chain.append(new PlainText(" | WLR: "));
-                    chain.append(new PlainText(decimalFormat.format(
-                            (float) bwJson.get("wins_bedwars").getAsInt() /
-                                    (float) bwJson.get("losses_bedwars").getAsInt())));
-                }
             } else {
                 chain.append(new PlainText("该玩家的起床战争数据为空"));
             }
