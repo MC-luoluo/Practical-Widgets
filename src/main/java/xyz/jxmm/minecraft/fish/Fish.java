bb4273ef8a3b580afdc84946aa088c57f9d0860e
MC洛洛 <1592859065@qq.com>
2024-07-03 15:25:43 +0800

    鱼、宝藏、垃圾
    简单测试没问题后提交

--- src/main/java/xyz/jxmm/minecraft/fish/Fish.java
+++ src/main/java/xyz/jxmm/minecraft/fish/Fish.java
@@ -11,47 +11,70 @@ import xyz.jxmm.minecraft.Nick;
 import java.text.DecimalFormat;
 
 public class Fish {
-    public static void fish(JsonObject json, Long sender, Group group){
+    public static void fish(JsonObject json, Long sender, Group group) {
         MessageChain at = MiraiCode.deserializeMiraiCode("[mirai:at:" + sender + "]");
         MessageChainBuilder chain = new MessageChainBuilder().append(at);
+        /*
         MessageChainBuilder all = new MessageChainBuilder();
         MessageChainBuilder water = new MessageChainBuilder();
         MessageChainBuilder lava = new MessageChainBuilder();
-        MessageChainBuilder ice = new MessageChainBuilder();
-
-        JsonObject playerJson = new JsonObject();
-        JsonObject statsJson;
-        JsonObject mainLobbyJson;
-        JsonObject fishJson;
-        boolean tf = false;
+        MessageChainBuilder ice = new MessageChainBuilder();*/
 
         DecimalFormat decimalFormat = new DecimalFormat("0.00");
 
-        if (!json.get("player").isJsonObject()) {
-            chain.append(new PlainText("无法得到 玩家相关信息"));
-        } else {
-            playerJson = json.get("player").getAsJsonObject();
-            if (playerJson.has("stats")){
-                statsJson = playerJson.get("stats").getAsJsonObject();
-                if (statsJson.has("MainLobby")){
-                    mainLobbyJson = statsJson.get("MainLobby").getAsJsonObject();
-                    if (mainLobbyJson.has("fishing")){
-                        fishJson = mainLobbyJson.get("fishing").getAsJsonObject();
-                        tf = true;
-                    } else chain.append(new PlainText("无法得到 钓鱼 数据!"));
-                } else chain.append(new PlainText("无法得到 主大厅 数据"));
+        //判断是否存在fishing数据
+        if (json.get("player").getAsJsonObject().has("stats") &&
+                json.get("player").getAsJsonObject().get("stats").getAsJsonObject().has("MainLobby") &&
+                json.get("player").getAsJsonObject().get("stats").getAsJsonObject().get("MainLobby").getAsJsonObject().has("fishing")) {
+            JsonObject playerJson = json.get("player").getAsJsonObject();
+            JsonObject fishJson = playerJson.get("stats").getAsJsonObject().get("MainLobby").getAsJsonObject().get("fishing").getAsJsonObject();
+
+            chain.append(new PlainText(Nick.nick(playerJson) + " "));
+            chain.append(new PlainText(json.get("player").getAsJsonObject().get("displayname").getAsString()));
+            chain.append(new PlainText(" | 大厅钓鱼数据:"));
+            if (fishJson.has("stats") && fishJson.get("stats").getAsJsonObject().has("permanent")) {
+                JsonObject permanent = fishJson.get("stats").getAsJsonObject().get("permanent").getAsJsonObject();
+                chain.append(new PlainText("\n钓到鱼: "));
+                int fish = 0;
+                if (permanent.has("water") && permanent.get("water").getAsJsonObject().has("fish")) {
+                    fish += permanent.get("water").getAsJsonObject().get("fish").getAsInt();
+                }
+                if (permanent.has("ice") && permanent.get("ice").getAsJsonObject().has("fish")) {
+                    fish += permanent.get("ice").getAsJsonObject().get("fish").getAsInt();
+                }
+                if (permanent.has("lava") && permanent.get("lava").getAsJsonObject().has("fish")) {
+                    fish += permanent.get("lava").getAsJsonObject().get("fish").getAsInt();
+                }
+                chain.append(new PlainText(String.valueOf(fish)));
+                chain.append(new PlainText("\n钓到宝藏: "));
+                int treasure = 0;
+                if (permanent.has("water") && permanent.get("water").getAsJsonObject().has("treasure")) {
+                    treasure += permanent.get("water").getAsJsonObject().get("treasure").getAsInt();
+                }
+                if (permanent.has("ice") && permanent.get("ice").getAsJsonObject().has("treasure")) {
+                    treasure += permanent.get("ice").getAsJsonObject().get("treasure").getAsInt();
+                }
+                if (permanent.has("lava") && permanent.get("lava").getAsJsonObject().has("treasure")) {
+                    treasure += permanent.get("lava").getAsJsonObject().get("treasure").getAsInt();
+                }
+                chain.append(new PlainText(String.valueOf(treasure)));
+                chain.append(new PlainText("\n钓到垃圾: "));
+                int junk = 0;
+                if (permanent.has("water") && permanent.get("water").getAsJsonObject().has("junk")) {
+                    junk += permanent.get("water").getAsJsonObject().get("junk").getAsInt();
+                }
+                if (permanent.has("ice") && permanent.get("ice").getAsJsonObject().has("junk")) {
+                    junk += permanent.get("ice").getAsJsonObject().get("junk").getAsInt();
+                }
+                if (permanent.has("lava") && permanent.get("lava").getAsJsonObject().has("junk")) {
+                    junk += permanent.get("lava").getAsJsonObject().get("junk").getAsInt();
+                }
+                chain.append(new PlainText(String.valueOf(junk)));
             }
-        }
-
-        if (!tf) {
-            group.sendMessage(chain.build());
         } else {
-            chain.append(new PlainText(Nick.nick(playerJson))); //玩家名称前缀
-            chain.append(new PlainText(playerJson.get("displayname").getAsString()));
-            chain.append(new PlainText(" | 钓鱼 数据如下: "));
-
-
-            group.sendMessage(chain.build());
+            chain.append(MiraiCode.deserializeMiraiCode("[mirai:at:" + sender + "]")).append(new PlainText(" 该玩家的钓鱼数据为空"));
+            //group.sendMessage(chain.build());
         }
+        group.sendMessage(chain.build());
     }
 }
